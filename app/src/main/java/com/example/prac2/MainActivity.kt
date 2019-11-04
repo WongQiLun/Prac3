a62b3a0d7bd85edda40bf0821ccafe1c40b060b8
WongQi <wongql-wp16@student.tarc.edu.my>
2019-11-04 09:25:15 +0800

    mesage

--- app/src/main/java/com/example/prac2/MainActivity.kt
+++ app/src/main/java/com/example/prac2/MainActivity.kt
@@ -3,33 +3,42 @@ package com.example.prac2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        val resultText: TextView = findViewById(R.id.result_text)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener { rollDice() }
         val countUpButton: Button = findViewById(R.id.btnCount)
         countUpButton.setOnClickListener{countUp()}
+
     }
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+
         //Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
         val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
 
     }
 
     private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-         val y = resultText.text.toString()
-        var x =y.toInt()
-        if( x< 6) x++
-        resultText.text = x.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+
     }
 }
