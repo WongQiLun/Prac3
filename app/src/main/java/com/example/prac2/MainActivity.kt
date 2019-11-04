ca37e8e4accda438b30c9a5ed162ecb2978f3383
WongQi <wongql-wp16@student.tarc.edu.my>
2019-11-04 08:37:12 +0800

    mesage

--- app/src/main/java/com/example/prac2/MainActivity.kt
+++ app/src/main/java/com/example/prac2/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         val resultText: TextView = findViewById(R.id.result_text)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.btnCount)
+        countUpButton.setOnClickListener{countUp()}
     }
     private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
@@ -22,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         val randomInt = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
 
+    }
 
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+         val y = resultText.text.toString()
+        var x =y.toInt()
+        if( x< 6) x++
+        resultText.text = x.toString()
     }
 }
